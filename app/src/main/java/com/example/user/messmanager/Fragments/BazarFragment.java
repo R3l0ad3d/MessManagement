97bb557a26b1d844d0b3a8da5797525ada647a58
R3l0ad3d <j0yr3l0ad3d@gmail.com>
2018-01-06 00:54:16 +0600

    initial project

---
+++ app/src/main/java/com/example/user/messmanager/Fragments/BazarFragment.java
@@ -0,0 +1,29 @@
+package com.example.user.messmanager.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.user.messmanager.R;
+
+
+public class BazarFragment extends Fragment {
+
+
+    public BazarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bazar, container, false);
+    }
+
+
+}
